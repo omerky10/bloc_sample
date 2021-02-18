9c7ed7c7042addff2dc15f30b0c09f94cbe01379
omerky10 <omer.kaya16@ogr.atauni.edu.tr>
2021-02-18 14:10:44 +0300

    Initial commit

---
+++ android/app/src/main/java/com/omerky/bloc_sample/MainActivity.java
@@ -0,0 +1,6 @@
+package com.omerky.bloc_sample;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/data/product_service.dart
@@ -0,0 +1,21 @@
+import 'package:bloc_sample/models/product.dart';
+
+class ProductService{
+  static List<Product> products ;
+
+  static ProductService _singleton = ProductService._internal();
+
+  factory ProductService (){
+    return _singleton;
+  }
+  ProductService._internal();
+
+  static List<Product> getAll(){
+    products.add(Product(1, "acer Laptop", 6000));
+    products.add(Product(2, "Lenovo Laptop", 500));
+    products.add(Product(3, "MSİ Laptop", 8000));
+    return products;
+  }
+
+
+}
